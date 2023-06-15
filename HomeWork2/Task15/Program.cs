8d820362bc9d286307c489eef62e7c6a07fe89c0
aerofotosintez <aerofotosintez@gmail.com>
2023-06-15 20:04:40 +0300

    добавил Task15 в HomeWork2

---
+++ HomeWork2/Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+Console.WriteLine("введите день недели числом ");
+string numberStringDay = Console.ReadLine()!;
+int numberIntDay = Convert.ToInt32(numberStringDay);
+
+if (numberIntDay == 6 || numberIntDay == 7)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
