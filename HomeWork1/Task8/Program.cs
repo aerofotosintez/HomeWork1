4de43a7354154842b5a1d5c4b697ab9314b5f55c
aerofotosintez <aerofotosintez@gmail.com>
2023-06-15 16:22:26 +0300

    Добавлено крайнее Д.з.

--- HomeWork1/Task8/Program.cs
+++ HomeWork1/Task8/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+Console.WriteLine("Введите число: ");
+string n = Console.ReadLine()!;
+int N = Convert.ToInt32(n);
+
+for (int a = 1; a <= N; a++)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+}
