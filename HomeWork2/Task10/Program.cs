ff1b6353f0ca11fd5f17ca582be8a53c17b895b1
aerofotosintez <aerofotosintez@gmail.com>
2023-06-16 17:23:05 +0300

    домашка 2

---
+++ HomeWork2/Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("введите трехзначное число ");
+string numberString = Console.ReadLine()!;
+int numberInt = Convert.ToInt32(numberString);
+Console.WriteLine((numberInt /10) % 10);
