d24e64e22052c33474e03c9ca303b5d2ee99a34c
aerofotosintez <aerofotosintez@gmail.com>
2023-07-01 16:15:13 +0300

    добавл Seminar4 task30

--- Semiar4/Task26/Program.cs
+++ Semiar4/Task26/Program.cs
@@ -4,3 +4,23 @@
 // 78 -> 2
 // 89126 -> 5
 
+int ReadInt()
+{
+    Console.WriteLine("Введе целое число: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int Count(int number)
+{
+    int i = 1;
+    while (number >= 10)
+    {
+        number = number / 10;
+        i++;
+    }
+    return i;
+}
+int number = ReadInt();
+int i = Count(number);
+Console.WriteLine($"количество цифр в введеном числе: {i}");
---
+++ Semiar4/Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает
+//  на вход число N и выдаёт произведение
+//  чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ReadInt()
+{
+    Console.WriteLine("Введе целое число: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int Multiply(int number)
+{
+int result = 1;
+    for (int i = 1; number >= i; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+int number = ReadInt();
+int multiply = Multiply(number);
+Console.WriteLine($"произведение чисел от 1 до {number} = {multiply}");
