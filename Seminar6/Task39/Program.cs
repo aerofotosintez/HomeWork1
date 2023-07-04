09250912519f66a5da2fd279efe3da83c3851b81
aerofotosintez <aerofotosintez@gmail.com>
2023-07-04 22:41:54 +0300

    добавил Seminar6

---
+++ Seminar6/Task39/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая перевернёт одномерный 
+// массив (последний элемент будет на первом месте, 
+// а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+// Console.WriteLine("введите длину массива: ");
+//  int n = Convert.ToInt32(Console.ReadLine());
+
+// int ReadInt(string message)
+// {
+//     Console.WriteLine(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+int[] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void ReverseArray(int[] surseArray)
+{
+    for(int i = 0; i < surseArray.Length / 2; i++)
+    {
+        int temp = surseArray[i];
+        surseArray[i] = surseArray[surseArray.Length - i - 1];
+        surseArray[surseArray.Length - i - 1] = temp;
+    }
+}
+
+int[] myArray = GetRandomArray(5, -9, 10);
+Console.WriteLine(string.Join(", ", myArray));
+ReverseArray(myArray);
+Console.WriteLine(string.Join(", ", myArray));
---
+++ Seminar6/Task42/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет преобразовывать
+//  десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string a = "" ;
+while(number != 0)
+{
+    int i = number % 2;
+    number /= 2;
+    a = i + a; 
+    // a+=i => a=a+i
+}
+Console.WriteLine(a);
