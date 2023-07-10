69a991ee155288fd61ddd0fbce4d7b45937aad59
aerofotosintez <aerofotosintez@gmail.com>
2023-07-10 12:54:59 +0300

    добавил Seminar8 Task53

---
+++ Seminar8/Task53/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+Console.WriteLine("введите количество строк:");
+int ROWS = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов:");
+int COLUMNS = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] GetRandomMatrix( int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = matrix[i, j] = Random.Shared.Next(1, 20);
+        }
+    }
+return matrix;
+}
+
+void SwapFirstAndLastRows(int[,] matrix)
+{
+        
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+       
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] myMatrix = GetRandomMatrix(ROWS, COLUMNS);
+PrintMatrix(myMatrix);
+Console.WriteLine();
+
+SwapFirstAndLastRows(myMatrix);
+PrintMatrix(myMatrix);
